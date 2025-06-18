95a62277425f9c2938f433984f5c2a12dcbb805b
razan-sameh <razansameh53@gmail.com>
2025-06-18 20:11:35 +0300

    change router way

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { router } from './routes'
-import { RouterProvider } from 'react-router'
+import { BrowserRouter, Route, Routes } from 'react-router'
+import App from './App'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
